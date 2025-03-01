be57d23628a8972ad3a63733eae309ec74f56b50
Yashar Hosseinpour <yashar.hv@gmail.com>
2025-03-01 20:10:36 +0100

    feat: add init_infra

---
+++ init_infra/terraform_backend.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.terraform_state_bucket
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = var.terraform_state_lock_dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "TerraformStateLocking"
+  }
+}
---
+++ init_infra/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+}
+
+variable "terraform_state_bucket" {
+  description = "The AWS S3 bucket used to store terraform state"
+  type        = string
+}
+
+variable "terraform_state_lock_dynamodb_table" {
+  description = "The AWS DynamoDB table name to be used for terraform state lock"
+  type        = string
+}
